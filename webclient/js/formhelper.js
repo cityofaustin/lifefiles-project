25dd53be9ccf72d744c61d98539478190724a2b4
jerryongit <jerome.peters@gmail.com>
2019-09-24 12:51:04 -0500

    add dynamic table row binding

--- webclient/js/formhelper.js
+++ webclient/js/formhelper.js
@@ -5,7 +5,8 @@
 
   mypass.formhelper = {
     showElement: showElement,
-    hideElement: hideElement
+    hideElement: hideElement,
+    bindTableRows:bindTableRows
   };
 
   function showElement(elementName) {
@@ -14,6 +15,26 @@
 
   function hideElement(elementName) {
     $(elementName).addClass('hidden');
+  }
+
+  function bindTableRows(tableClassName,dataRows) {
+    var str = new mypass.utils.StringBuilder();
+      var rowTemplate = $(tableClassName + ' tbody tr:first')[0].outerHTML;
+      var rowTemplateHlder = document.createComment(rowTemplate);
+
+      $(tableClassName + ' tbody').empty();
+      $(tableClassName + ' tbody').append(rowTemplateHlder);
+      var temp = $(rowTemplate).clone();
+      for (var index = 0; index < dataRows.length; index++) {
+        const element = dataRows[index];
+        var chrd = temp.children();
+        for (var index = 0; index < chrd.length; index++) {
+          var child = chrd[index];
+          var prop = child.getAttribute('data-bind');
+          child.innerText = element[prop];
+        }
+        $(tableClassName +' tbody').append(temp);
+      }
   }
 
 
--- webclient/ui/features/admin/serviceproviders/sp.js
+++ webclient/ui/features/admin/serviceproviders/sp.js
@@ -16,7 +16,6 @@
   function loadScreen(parentElement) {
     //ADD ANY PAGE CODE
     $('.btn-logout').removeClass('hidden');
-
     $(parentElement).empty();
     $(parentElement).append(mypass.admindashboard.serviceproviders.template);
     mypass.datacontext.serviceprovider.getAll().then(OnGetSps);
@@ -25,46 +24,7 @@
 
   function OnGetSps(res) {
     if (res.success) {
-      var dd = res.Rows;
-
-      var str = new mypass.utils.StringBuilder();
-      var rowTemplate = $('.sp-list table tbody tr:first')[0].outerHTML;
-      var rowTemplateHlder = document.createComment(rowTemplate);
-      
-      $('.sp-list table tbody').empty();
-      $('.sp-list table tbody').append(rowTemplateHlder);
-      
-      var temp = $(rowTemplate).clone();
-
-      for (var index = 0; index < res.Rows.length; index++) {
-        const element = res.Rows[index];
-
-        var chrd = temp.children();
-        for (var index = 0; index < chrd.length; index++) {
-          var child = chrd[index];
-          var prop = child.getAttribute('data-bind').split('.')[1];
-          child.innerText=element[prop];
-        }
-
-      $('.sp-list table tbody').append(temp);
-      }
-
-      // $('.sp-list table tbody').empty();
-      // $('.sp-list table tbody').append(str.toString());
-      
-//       accountid: 13
-// address: "Austin, Tx"
-// company_name: "mmmmmmmmmmm"
-// name: "Jerry Service provider"
-// primarykey: 1
-
-      // <tr>
-      //       <th scope="row">1</th>
-      //       <td>Mark</td>
-      //       <td>Otto</td>
-      //       <td>@mdo</td>
-      //     </tr>
-          
+      mypass.formhelper.bindTableRows('.sp-list',res.Rows);
     }
   }
 
