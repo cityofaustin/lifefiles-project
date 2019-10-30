f0a4918427af304d5bfb4716c2f2ca86202e7d8b
jerryongit <jerome.peters@gmail.com>
2019-10-30 14:30:31 -0500

    update controllers

--- server/controllers/api/owner.js
+++ server/controllers/api/owner.js
@@ -4,14 +4,25 @@ var
   util = require("util"),
   passportConf = require('../../config/passport'),
   _ = require('lodash'),
-  common = require("../../common")
+  common = require("../../common"),
+  multer = require('multer')
   ;
 
+//IF WE DECIDE TO HANDLE FILE POSTS IN RESPECTIVE CONTROLLERS AND NOT USE A GENERICE UPLOAD SERVICE
+//UNCOMMENT THE FOLLOWING CODE
+// var theFilePath = __dirname + '../../uploadtmp';
+// var uploadHandlerReportTemplate = multer({ dest: theFilePath });
 
 exports.init = function (app) {
   app.post('/owner/save', passportConf.isAuthenticated, SaveProfile);
-  // app.post('/owner/delete', passportConf.isAuthenticated, DeleteAccount);
-  // app.post('/account/changepassword', passportConf.isAuthenticated, ChangePassword);
+  app.post('/owner/getdocs', passportConf.isAuthenticated, GetDocs);
+  app.post('/owner/getfile', passportConf.isAuthenticated, GetFile);
+
+
+  //IF WE DECIDE TO HANDLE FILE POSTS IN RESPECTIVE CONTROLLERS AND NOT USE A GENERICE UPLOAD SERVICE
+  //UNCOMMENT THE FOLLOWING CODE
+  // app.post('/owner/documentupload', passportConf.isAuthenticated, uploadHandlerReportTemplate.array('thefile'), OnUpload);
+
 };
 
 function SaveProfile(req, res, next) {
@@ -23,14 +34,63 @@ function SaveProfile(req, res, next) {
   });
 }
 
-// function DeleteAccount(req, res, next) {
-//   //FOR DEMO PURPOSES ONLY...WONT USE IN PRODUCTION
-//   var data = {};
-//   // data.Profile = req.body.data;
-//   data.primarykey = req.User.accountid;
-//   bll.owner.DeleteAccount(data).then(function (bllRes) {
-//     res.status(200).send(bllRes);
-//   });
+function GetDocs(req, res, next) {
+  var data = {
+    ownerid: req.User.AccountInfo.primarykey
+  };
+  bll.owner.GetDocs(data).then(function (bllRes) {
+    res.status(200).send(bllRes);
+  });
+}
+
+function GetFile(req, res, next) {
+  var data = {
+    primarykey: req.body.data.primarykey,
+    thefile: req.body.data.thefile
+  };
+  bll.owner.GetFile(data).then(function (bllRes) {
+    res.status(200).send(bllRes);
+  });
+}
+
+
+//IF WE DECIDE TO HANDLE FILE POSTS IN RESPECTIVE CONTROLLERS AND NOT USE A GENERICE UPLOAD SERVICE
+//UNCOMMENT THE FOLLOWING CODE
+
+// function OnUpload(req, res, next) {
+//   //todo: add validation
+//   var response = new common.response();
+
+//   if (!req.files) {
+//     response.success = false;
+//     res.status(200).send(response);
+//     return;
+//   }
+
+
+//   for (var x = 0; x < req.files.length; x++) {
+//     var data = {
+//       FileName: req.body.FileName,
+//       FileType: req.body.FileType,
+//       MIMEType: req.body.MIMEType,
+//       fileInfo: req.files[x],
+//       User: req.User
+//     };
+
+//     // req.User.AccountInfo.userrole // tells us their role so we know how to handle the file
+//     //if we switch to buffers only and not write temp file to disk
+//     // data.File = req.files[x].buffer;
+
+//     bll.owner.saveDocument(data).then(function (tempInsertRes) {
+//       if (!tempInsertRes.success) {
+//         logger.log('api.upload err ' + util.inspect(tempInsertRes));
+//         response.success = false;
+//       }
+//       else {
+//         response.success = true;
+//       }
+//       res.status(200).send(response);
+//       return;
+//     });
+//   }
 // }
-
-
