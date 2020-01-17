87854f85ce8c03b40dc88eee991886e0d9fb5ada
roesslern <roesslern@OOI-3ALLVDL-M1.local>
2020-01-17 10:23:24 -0600

    uport integration with new eth public/priv keys on each run

--- server/services/uport/index.js
+++ server/services/uport/index.js
@@ -1,5 +1,5 @@
 var EthrDID = require('ethr-did');
-// var Issuer = require('did-jwt-vc').Issuer;
+var Web3 = require('web3');
 var createVerifiableCredential = require('did-jwt-vc').createVerifiableCredential;
 var createPresentation = require('did-jwt-vc').createPresentation;
 var Resolver = require('did-resolver').Resolver
@@ -14,18 +14,38 @@ function uport() {
     function init() {
         console.log('UPort Init')
         createAndVerify();
-        console.log('\n\nUPort Create And Verify Workflow Finished\n\n')
     }
 
+    function createNewDID() {
+        var web3 = new Web3();
+        var account = web3.eth.accounts.create();
+        let privKeyWithoutHeader = account.privateKey.substring(2);
+        const ethrDid = new EthrDID({address: account.address, privateKey: privKeyWithoutHeader})
+        return ethrDid;
+    }
+
+    // function createIssuer() {
+    //     const issuer = new EthrDID({
+    //         address: '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
+    //         privateKey: 'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75'
+    //     });
+    //     return issuer;
+    // }
+
     async function createAndVerify() {
-        const issuer = createIssuer();
+
+        let issuerDID = createNewDID();
+        let bachelorDegreeDID = createNewDID();
+        let issueTime = 1562950282;
+
+        // const issuer = createIssuer();
         const resolver = new Resolver(getResolver())
         
-        const vcJwt = await createVC(issuer);
+        const vcJwt = await createVC(issuerDID, bachelorDegreeDID, issueTime);
         console.log('\n\nVerifiable Credential Created!\n')
         console.log(vcJwt)
 
-        const vpJwt = await creatingVerifiablePresentation(issuer, vcJwt);
+        const vpJwt = await creatingVerifiablePresentation(issuerDID, vcJwt);
         console.log('\n\nVerifiable Presentation Created!\n')
         console.log(vpJwt)
 
@@ -36,21 +56,15 @@ function uport() {
         const verifiedPR = await verifyVP(vpJwt, resolver)
         console.log('\n\nVerifiable Presentation Has Been Verified!\n')
         console.log(verifiedPR)
+
+        console.log('\n\nUPort Create And Verify Workflow Finished\n\n')
+
     }
 
-    function createIssuer() {
-        const issuer = new EthrDID({
-            address: '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
-            privateKey: 'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75'
-        });
-
-        return issuer;
-    }
-
-    async function createVC(issuer) {
+    async function createVC(issuer, bachelorDegreeDID, issueTime) {
         const vcPayload = {
-            sub: 'did:ethr:0x435df3eda57154cf8cf7926079881f2912f54db4',
-            nbf: 1562950282,
+            sub: bachelorDegreeDID.did,
+            nbf: issueTime,
             vc: {
                 '@context': ['https://www.w3.org/2018/credentials/v1'],
                 type: ['VerifiableCredential'],
