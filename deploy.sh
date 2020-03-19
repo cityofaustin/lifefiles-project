ffbda4134e4360766185d71b33f6e8e302c26a1c
roesslern <groessler24@gmail.com>
2020-03-19 15:06:59 -0500

    updating deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ mkdir CIRCLE-CI-MYPASS-BACKEND || echo Directory Exists, Continuing...
 cd CIRCLE-CI-MYPASS-BACKEND
 git clone https://github.com/cityofaustin/mypass.git || echo Already Cloned, Continuing...
 
-cd mypass-backend
+cd mypass
 git pull
 
 docker-compose down
