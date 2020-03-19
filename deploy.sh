a9d79441c5b1cf7ee66c58de493e4a3fa3cf9484
roesslern <groessler24@gmail.com>
2020-03-19 14:39:30 -0500

    Adding deploy

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,9 @@ mkdir CIRCLE-CI-MYPASS-BACKEND || echo Directory Exists, Continuing...
 
 
 cd CIRCLE-CI-MYPASS-BACKEND
-git clone https://github.com/nitro-neal/mypass-backend.git || echo Already Cloned, Continuing...
+git clone https://github.com/cityofaustin/mypass.git || echo Already Cloned, Continuing...
 
 cd mypass-backend
 git pull
-
-yarn
-
-forever stop app.js
-forever start -c "node" app.js
 
 echo ~~FINISHED~~
