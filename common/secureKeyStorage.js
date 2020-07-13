1a495f4c58ca99372a3f0ccae7286c69497ff413
nitro-neal <groessler24@gmail.com>
2020-07-13 11:16:02 -0500

    :metal: Changing production to use encrypted db storage for keys

--- common/secureKeyStorage.js
+++ common/secureKeyStorage.js
@@ -1,11 +1,9 @@
 const { FileSafe } = require("./FileSafe");
 const common = require("../common/common");
 const Crypto = require("crypto");
-let vault;
 let safe;
 
-if (process.env.ENVIRONMENT === "HEROKU") {
-} else if (process.env.ENVIRONMENT === "DEVELOPMENT") {
+if (process.env.ENVIRONMENT === "DEVELOPMENT") {
   safe = new FileSafe("safe.dat", process.env.FILE_SAFE_KEY);
   try {
     safe.decrypt();
@@ -14,59 +12,24 @@ if (process.env.ENVIRONMENT === "HEROKU") {
     let emptyJson = {};
     safe.encrypt(emptyJson);
   }
-} else {
-  vault = require("node-vault")({ endpoint: "http://myvault:8200" });
-  if (process.env.VAULT_KEY) {
-    vault.unseal({ secret_shares: 1, key: process.env.VAULT_KEY });
-    vault.token = process.env.VAULT_ROOT_TOKEN;
-  } else {
-    vault
-      .init({ secret_shares: 1, secret_threshold: 1 })
-      .then((result) => {
-        console.log("Inital Keys:");
-        console.log(result);
-
-        var keys = result.keys;
-        // set token for all following requests
-        vault.token = result.root_token;
-        // unseal vault server
-        vault.unseal({ secret_shares: 1, key: keys[0] }).then((sealRes) => {
-          console.log(sealRes);
-          vault.mount({
-            mount_point: "secret",
-            type: "generic",
-            description: "keys",
-          });
-        });
-      })
-      .catch(console.error);
-  }
 }
 
 module.exports = {
   store: async (guid, key) => {
-    if (process.env.ENVIRONMENT === "HEROKU") {
-      this.storeToDb(guid, key);
-    } else if (process.env.ENVIRONMENT === "DEVELOPMENT") {
+    if (process.env.ENVIRONMENT === "DEVELOPMENT") {
       let data = safe.decrypt();
       data[guid] = key;
       safe.encrypt(data);
     } else {
-      await vault.write("secret/" + guid, { value: key });
+      this.storeToDb(guid, key);
     }
   },
   retrieve: async (guid) => {
-    if (process.env.ENVIRONMENT === "HEROKU") {
-      return this.retrieveFromDb(guid);
-    } else if (
-      process.env.ENVIRONMENT === "DEVELOPMENT" ||
-      process.env.ENVIRONMENT === "HEROKU"
-    ) {
+    if (process.env.ENVIRONMENT === "DEVELOPMENT") {
       let data = safe.decrypt();
       return data[guid];
     } else {
-      const value = await vault.read("secret/" + guid);
-      return value.data.value;
+      return this.retrieveFromDb(guid);
     }
   },
 
